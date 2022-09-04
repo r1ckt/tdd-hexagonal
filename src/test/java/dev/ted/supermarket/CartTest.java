97516b154ab6b2218052a394e7ee1f8a345acd47
Ricardo Olivera <rholivera93@gmail.com>
2022-09-04 01:38:41 -0300

    two scenarios pass

---
+++ src/test/java/dev/ted/supermarket/CartTest.java
@@ -0,0 +1,24 @@
+package dev.ted.supermarket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CartTest {
+
+  @Test
+  void emptyCartHasTotalPriceOfZero() {
+    Cart cart = new Cart();
+
+    assertThat(cart.totalPrice()).isZero();
+  }
+
+  @Test
+  public void addToothbrushProductThenTotalPriceIsOneDollar(){
+    Cart cart = new Cart();
+    cart.add("Toothbrush", 1);
+
+    assertThat(cart.totalPrice()).isEqualTo(1);
+  }
+
+}
